a6779ca3b45a7459679b26ecfdf0e829f8d16efc
Usama26 <usama.waseem96@gmail.com>
2020-02-21 17:03:40 +0500

    Done For Tribune And Dawn

--- news_project/news_project/spiders/links_spider.py
+++ news_project/news_project/spiders/links_spider.py
@@ -2,18 +2,28 @@ import scrapy
 
 class LinksSpider(scrapy.Spider):
     name = "links"
+    start_urls = [
+        'https://tribune.com.pk/pakistan',
+        'https://dawn.com/pakistan',
+    ]
 
-    def start_request(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url ,callback = self.parse)
+    def parse(self, response):
+        final = []
+        website_name = response.url.split("https://")[1].split(".com")[0]
+        
+        if(website_name == 'tribune'):
+            targets = response.css('.cat-1.group-0 .title a')
+            links = targets.xpath("@href").extract()[:2]
+            final.append({
+                "website":website_name,
+                "links" : links
+            })
 
-     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        elif(website_name == 'www.dawn'):
+            targets = response.css('.mb-4 > .story .story__link')
+            links = targets.xpath("@href").extract()[:2]
+            final.append({
+                "website":website_name.split("www.")[1],
+                "links" : links
+            })
+        yield final[0] 
